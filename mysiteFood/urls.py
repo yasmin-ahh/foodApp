42545eac4f82c80818670ed557b7ae5fcdc0be91
Yasmin <yasminhassan240@gmail.com>
2022-12-03 21:18:20 +0400

    fixing profile image display

--- mysiteFood/urls.py
+++ mysiteFood/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/', user_views.register, name='register'),
@@ -26,3 +28,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profilepage, name='profile'),
 ]
+
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
