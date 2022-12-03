ab52e70e32c6450d64c9dff81a2eaed2fa84cd6f
Yasmin <yasminhassan240@gmail.com>
2022-12-03 20:24:30 +0400

    adding profile model to include more data about user

--- mysiteFood/settings.py
+++ mysiteFood/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'food:index'
 LOGIN_URL = 'login' #to not allow django to go to /accounts/login/ redirect
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures') #path to the root directory where files are stored
+# for hosted websites images are usually stored on the clod e.g: Amazon S3 buckets
+MEDIA_URL = '/pictures/'
