380a3105db3b61f8a814c9d76081070237e3de2c
Yasmin <yasminhassan240@gmail.com>
2022-12-03 19:55:56 +0400

    restricting routes to view the profile page

--- mysiteFood/urls.py
+++ mysiteFood/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profilepage, name='profile'),
 ]
