ceabfa1fd66dbb03fb52d9abf6a0dad0d57a5997
Yasmin <yasminhassan240@gmail.com>
2022-12-11 21:01:13 +0400

    add user as a foriegn key to items created

--- food/models.py
+++ food/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Item(models.Model):
 
     def __str__(self):
         return self.item_name
-
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
     item_img = models.CharField(max_length=500, default="https://cdn-icons-png.flaticon.com/512/1147/1147805.png")
+
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
