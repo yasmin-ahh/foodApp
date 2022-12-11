de94b7a3b99129da20eac1c23edf2bf6254bf8d2
Yasmin <yasminhassan240@gmail.com>
2022-12-11 21:01:59 +0400

    automatically associate user with item created

--- food/urls.py
+++ food/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path('', views.IndexClassView.as_view(), name='index'),
     path('item/', views.item, name='item'),
     path('<int:pk>/',views.FoodDetail.as_view(), name='detail'),
-    path('add/',views.create_item, name='create_item'),
+    # path('add/',views.create_item, name='create_item'),
+    path('add/',views.CreateItem.as_view(), name='create_item'),
     #edit item
     path('update/<int:id>', views.update_item, name='update_item'),
     path('delete/<int:id>', views.delete_item, name='delete_item')
--- food/views.py
+++ food/views.py
@@ -6,7 +6,7 @@ from .models import Item
 from django.template import loader
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
+from django.views.generic.edit import CreateView
 class IndexClassView(ListView):
     model = Item
     template_name = 'food/index.html'
@@ -43,6 +43,16 @@ def create_item(request):
         form.save()
         return redirect('food:index')
     return render(request, 'food/item-form.html', {'form': form})
+
+#This is a class based view for create item
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_img']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
 
 def update_item(request, id):
     item = Item.objects.get(id=id)
